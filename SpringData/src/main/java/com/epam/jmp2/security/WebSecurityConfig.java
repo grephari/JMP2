05c37ac6ad77c2468f99da6853096b2f15667f0e
BrunoWei <994962084@qq.com>
2017-01-10 17:43:30 +0800

    secure add accident operation

--- SpringData/src/main/java/com/epam/jmp2/security/WebSecurityConfig.java
+++ SpringData/src/main/java/com/epam/jmp2/security/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/webapp/home").permitAll()
 		                        .antMatchers("/webapp/list").hasAnyAuthority("USER", "ADMIN")
+		                        .antMatchers("/webapp/add").hasAuthority("ADMIN")
 		                        .anyRequest().authenticated()
 		                        .and().formLogin().loginPage("/login").permitAll()
 		                        .and().logout().permitAll();
